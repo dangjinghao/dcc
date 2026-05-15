b89accb97494e4e2e8abfecc1948248d1d003d90
dangjinghao <dangjinghaoemail@163.com>
2026-05-15 23:47:31 +0800

    Refactor testcase

--- test/type.c
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-
-int main() {
-  assert((sizeof(~(char)1) == sizeof(int)));
-  assert((sizeof(~(long)1) == sizeof(long)));
-  assert((sizeof(~(char)1 >> 1) == sizeof(int)));
-  assert((sizeof(~(long)1 >> 1) == sizeof(long)));
-}
