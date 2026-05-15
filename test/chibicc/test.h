508b5a7aac2b64620e3f6d48aa66ec12b4703533
dangjinghao <dangjinghaoemail@163.com>
2026-05-15 21:15:24 +0800

    Add: testcase

---
+++ test/chibicc/test.h
@@ -0,0 +1,22 @@
+#define ASSERT(x, y) assert(x, y, #y)
+
+#define alloca(x) __builtin_alloca(x)
+
+int printf(char *fmt, ...);
+int sprintf(char *buf, char *fmt, ...);
+int vsprintf(char *buf, char *fmt, void *ap);
+int strcmp(char *p, char *q);
+int strncmp(char *p, char *q, long n);
+int memcmp(char *p, char *q, long n);
+void exit(int n);
+int vsprintf();
+long strlen(char *s);
+void *memcpy(void *dest, void *src, long n);
+void *memset(void *s, int c, long n);
+
+static inline void assert(int expected, int actual, char *code) {
+  if (expected != actual) {
+    printf("assert failed: %s\n", code);
+    exit(1);
+  }
+}
