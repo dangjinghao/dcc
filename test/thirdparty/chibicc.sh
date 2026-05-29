5059ecf69d72f6d453e91c41471fa5f1969fe056
dangjinghao <dangjinghaoemail@163.com>
2026-05-30 01:24:28 +0800

    Add some passed thirdparty test

---
+++ test/thirdparty/chibicc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+repo='https://github.com/rui314/chibicc.git'
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+. "$SCRIPT_DIR/common"
+
+git reset --hard 90d1f7f199cc55b13c7fdb5839d1409806633fdb
+
+$make clean
+$make CC=$dcc
+$make test
---
+++ test/thirdparty/libpng.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+repo='https://github.com/rui314/libpng.git'
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+. "$SCRIPT_DIR/common"
+
+git reset --hard dbe3e0c43e549a1602286144d94b0666549b18e6
+
+CC=$dcc ./configure
+sed -i 's/^wl=.*/wl=-Wl,/; s/^pic_flag=.*/pic_flag=-fPIC/' libtool
+$make clean
+$make
+$make test
