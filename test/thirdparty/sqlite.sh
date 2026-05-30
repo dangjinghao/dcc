3e12fd860244e61b67639fc497938a190feb2c31
dangjinghao <dangjinghaoemail@163.com>
2026-05-30 16:05:00 +0800

    Add passed sqlite testcase

---
+++ test/thirdparty/sqlite.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+repo='https://github.com/sqlite/sqlite.git'
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+. "$SCRIPT_DIR/common"
+
+git reset --hard 86f477edaa17767b39c7bae5b67cac8580f7a8c1
+
+CC=$dcc CFLAGS=-D_GNU_SOURCE ./configure
+sed -i 's/^wl=.*/wl=-Wl,/; s/^pic_flag=.*/pic_flag=-fPIC/' libtool
+$make clean
+$make
+$make test
